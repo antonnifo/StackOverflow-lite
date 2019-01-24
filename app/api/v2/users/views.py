1fc4800031ebc9dd45ec9f6c401a66655bbd5776
antonnifo <kenyattaanthony88@gmail.com>
2019-01-24 19:37:03 +0300

    add signin class

--- app/api/v2/users/views.py
+++ app/api/v2/users/views.py
@@ -5,6 +5,12 @@ from flask import jsonify,request
 from flask_restful import Resource
 from .models import UserModel
 
+def nonexistent_user():
+    return jsonify({
+        "status": 404,
+        "message": "user does not exist"
+    })
+
 class UserSignUp(Resource):
     """Class with user signup post method"""
 
@@ -31,6 +37,35 @@ class UserSignUp(Resource):
                 {
                     "account details": user_details,
                     "message": "You have created an account you can now sign in"
+                }
+            ]
+        })
+
+
+class UserSignIn(Resource):
+    """Class containing user login method"""
+
+    def __init__(self):
+        self.db = UserModel()
+
+    def post(self):
+        """method to get a specific user"""
+        user = self.db.log_in()
+        if user is None:
+            return nonexistent_user()
+
+        if user == 'incorrect password':
+            return jsonify({
+                "status": 401,
+                "message": "password or email is incorrect please try again"
+            })
+
+        return jsonify({
+            "status": 200,
+            "data": [
+                {
+                    "user": user,
+                    "message": "You are now signed in you can post your question"
                 }
             ]
         })
