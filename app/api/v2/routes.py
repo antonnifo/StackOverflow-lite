bccacdc3101eb6aeaeda3260bbb536eb4051e041
antonnifo <kenyattaanthony88@gmail.com>
2019-01-24 19:38:12 +0300

    regester signin route

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -2,9 +2,10 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .users.views import UserSignUp
+from .users.views import UserSignUp, UserSignIn
 
 VERSION_DOS = Blueprint('apiv2', __name__, url_prefix='/api/v2')
 API = Api(VERSION_DOS)
 
 API.add_resource(UserSignUp, '/auth/signup')
+API.add_resource(UserSignIn, '/auth/signin')
