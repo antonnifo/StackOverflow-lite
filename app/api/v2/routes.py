74124c73cdfcc1343a435be07aad4c0707314bb7
antonnifo <kenyattaanthony88@gmail.com>
2019-01-25 12:59:41 +0300

    regester questions class

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -2,10 +2,12 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .users.views import UserSignUp, UserSignIn
+from app.api.v2.users.views import UserSignUp, UserSignIn
+from app.api.v2.questions.views import Questions
 
 VERSION_DOS = Blueprint('apiv2', __name__, url_prefix='/api/v2')
 API = Api(VERSION_DOS)
 
 API.add_resource(UserSignUp, '/auth/signup')
 API.add_resource(UserSignIn, '/auth/signin')
+API.add_resource(Questions, '/questions')
