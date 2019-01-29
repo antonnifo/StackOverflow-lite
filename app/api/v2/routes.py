c9016cda78b685e5b0a3a5d43b4522feb14f088f
antonnifo <kenyattaanthony88@gmail.com>
2019-01-29 21:29:10 +0300

    regester answer quiz route

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 
 from app.api.v2.users.views import UserSignUp, UserSignIn
 from app.api.v2.questions.views import Questions
+from app.api.v2.answers.views import Answers
 
 VERSION_DOS = Blueprint('apiv2', __name__, url_prefix='/api/v2')
 API = Api(VERSION_DOS)
@@ -11,3 +12,4 @@ API = Api(VERSION_DOS)
 API.add_resource(UserSignUp, '/auth/signup')
 API.add_resource(UserSignIn, '/auth/signin')
 API.add_resource(Questions, '/questions')
+API.add_resource(Answers, '/questions/<int:question_id>/answers')
