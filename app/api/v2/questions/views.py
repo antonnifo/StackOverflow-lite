fd745abf0e261cca09d00e6308c8d1eaa037ca9f
antonnifo <kenyattaanthony88@gmail.com>
2019-02-01 10:38:53 +0300

    add endpoint to delete quiz

--- app/api/v2/questions/views.py
+++ app/api/v2/questions/views.py
@@ -5,6 +5,7 @@ from flask_restful import Resource
 from app.api.v2.questions.models import QuestionsModel
 from app.api.v2.token_decorator import require_token
 from app.api.v2.users.models import UserModel
+from app.api.validators import non_existance_question, only_creater_can_delete
 
 
 class Questions(Resource):
@@ -58,5 +59,21 @@ class Question(Resource):
         questions = self.db.find_quiz_answers(question_id)
         return jsonify({
             "status": 200,
-            "data": questions
-        })     
+            "question": questions
+        })
+
+    @require_token
+    def delete(current_user, self, question_id):
+        """view method for deleting a quiz and all its answers"""
+        question = self.db.find_quiz_by_id(question_id)
+        if question is None:
+            return non_existance_question()
+
+        if current_user["user_id"] != question["user_id"]:
+            return only_creater_can_delete()
+
+        if self.db.delete(question_id) == "deleted":
+            return jsonify({
+                "status": 200,
+                "message": 'you have deleted your question'
+            })         
