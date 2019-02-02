b3ae7e867f4db723eea29865e6a70444419a18dc
antonnifo <kenyattaanthony88@gmail.com>
2019-02-02 13:02:37 +0300

    allow author of a quiz to accept an answer

--- app/api/v2/answers/views.py
+++ app/api/v2/answers/views.py
@@ -2,7 +2,6 @@
 from flask import jsonify, request
 from flask_restful import Resource
 
-from app.api.v2.questions.models import QuestionsModel
 from app.api.v2.token_decorator import require_token
 from app.api.v2.users.models import UserModel
 from app.api.v2.answers.models import AnswerModel
@@ -37,7 +36,7 @@ class Answers(Resource):
         })
 
 class UpdateAnswer(Resource):
-    """view class for updating a quiz"""
+    """view class for updating an answer"""
 
     def __init__(self):
         """
@@ -51,7 +50,7 @@ class UpdateAnswer(Resource):
     @require_token
     def patch(current_user, self, answer_id):
         """method to update an answer"""
-        answer = self.db.ffind_answer_by_id(
+        answer = self.db.find_answer_by_id(
             answer_id)
         if answer is None:
             return non_existance_question()
@@ -68,5 +67,44 @@ class UpdateAnswer(Resource):
                 "data": {
                     "id": answer_id,
                     "message": "Updated your answer"
+                }
+            })
+
+
+class AcceptAnswer(Resource):
+    """view class for accepting an answer"""
+
+    def __init__(self):
+        """
+        executes when the class is being initiated
+        used to assign values to object properties
+        self parameter is a reference to tha class instance itself & is used 
+        to access variables that belong to that class
+        """
+        self.db = QuestionsModel()
+
+    @require_token
+    def patch(current_user, self, answer_id):
+        """method to update an answer"""
+        answer = self.db.find_answer_by_id(
+            answer_id)
+        if answer is None:
+            return non_existance_question()
+
+        if current_user["user_id"] != self.db.get_quiz_author(answer_id):
+            return jsonify({
+            "status": 401,
+            "error": "sorry you can't accept me lol"
+})
+
+        edit_status = self.db.accept_answer(
+            answer_id)
+
+        if edit_status == "answer accepted":
+            return jsonify({
+                "status": 200,
+                "data": {
+                    "id": answer_id,
+                    "message": "you accepted the above as your prefered answer"
                 }
             })
